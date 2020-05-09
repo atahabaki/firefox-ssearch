f6a81ffb1b868812d699de21b463e24e1dbfface
A. Taha Baki <a.taha.baki@outlook.com>
2020-05-10 00:47:27 +0300

    v4.1

---
+++ README.md
@@ -0,0 +1,112 @@
+# firefox-smartbox
+
+`smartbox v4.1`
+
+Searching would not be much simpler. Just type "@" in URL bar. Then type the search engine you wanna search e.g. 'ytb', 'ggl', 'bng' etc. Then type the search string.
+
+Use it well...
+
+## Available Search Engines & Keywords
+
+| Platform     | Keyword   |
+|--------------|-----------|
+| YouTube      | ytb       |
+| Google       | ggl       |
+| Bing         | bng       |
+| DuckDuckGo   | dckgo     |
+| SesliSozluk  | sszlk     |
+| Yahoo        | yaho      |
+| TorrentOyun  | trryn     |
+| Yandex       | ydex      |
+| AllItEBooks  | itebx     |
+| Iconfinder   | iconf     |
+| Reddit       | rddt      |
+| TorrentMafya | trrmf     |
+| GitHub       | gthb      |
+
+## Usage
+
+### JSON data...
+
+```json
+[
+    {
+        "name": "xmpl",
+        "desc": "example",
+        "url": "https://example.com/search/?q={%query%}",
+    }
+]
+```
+
+### Searching
+
+Simple to search on whatever you want.
+
+```
+@ <keyword> <search_query>
+```
+
+keyword: is the short version of the web site's name.
+search\_query: search query, nothing special. Just I need to mention here it'll be encoded.
+
+### Adding a search engine
+
+Adding search engine to smartbox is simple. Please just type:
+
+```
+@ +<keyword> <site_name> <url>
+```
+
+keyword: is the short-name of the web site
+site\_name: is the name of the web site
+url: can be similar to this one: "https://example.com/search?q="
+
+### Removing a search engine
+
+Removing a search engine from database as simple as adding one. Please just type:
+
+```
+@ -<keyword>
+```
+
+keyword: is the short-name of the web site.
+
+### Listing all available search engines
+
+There're 2 options you can try to list all of them. The first one is:
+
+```
+@ .
+```
+
+You can list just specific amount of them by typing "?".
+
+The other one is my favorite. You can quickly review what's available in comma seperated form. Try:
+
+```
+@ ...
+```
+
+Which one is better?
+
+### Importing, Exporting and Adding sites
+
+Importing, easily pasting prepared JSON array of search engines...
+```
+@ <= <json_array>
+```
+
+Exporting, easly while on omnibox. :D
+```
+@ >=
+```
+
+Adding array of search engines, it's now easier. :D
+```
+@ +<= <json_array>
+
+## ChangeLog
+
+* Storing changes locally. (Finally)
+* Importing, Exporting and Adding sites (Experimental, but yes :D)
+* General improvements and fixes.
---
+++ app.js
@@ -0,0 +1,242 @@
+var sites = [
+    {
+        "name":"ytb",
+        "desc":"YouTube",
+        "url":"https://www.youtube.com/results?search_query={%query%}"
+    },
+    {
+        "name":"ggl",
+        "desc":"Google",
+        "url":"https://www.google.com/search?q={%query%}"
+    },
+    {
+        "name":"dvand",
+        "desc": "Android Devs",
+        "url": "https://developer.android.com/s/results?q={%query%}"
+    },
+    {
+        "name":"bng",
+        "desc":"Bing",
+        "url":"https://www.bing.com/search?q={%query%}"
+    },
+    {
+        "name":"dckgo",
+        "desc":"DuckDuckGo",
+        "url":"https://duckduckgo.com/?q={%query%}"
+    },
+    {
+        "name":"sszlk",
+        "desc":"SesliSözlük",
+        "url":"https://seslisozluk.net/{%query%}"
+    },
+    {
+        "name":"yaho",
+        "desc":"Yahoo",
+        "url":"https://search.yahoo.com/?q={%query%}"
+    },
+    {
+        "name":"trryn",
+        "desc":"Torrent Oyun İndir",
+        "url":"https://www.torrentoyunindir.com/?s={%query%}"
+    },
+    {
+        "name":"ydex",
+        "desc":"Yandex",
+        "url":"https://yandex.com/search/?text={%query%}"
+    },
+    {
+        "name":"itebx",
+        "desc":"All IT e-Books",
+        "url":"http://allitebooks.org/?s={%query%}"
+    },
+    {
+        "name":"iconf",
+        "desc":"IconFinder",
+        "url":"https://www.iconfinder.com/search/?q={%query%}"
+    },
+    {
+        "name":"rddt",
+        "desc":"Reddit",
+        "url":"https://www.reddit.com/search/?q={%query%}"
+    },
+    {
+        "name":"trrmfy",
+        "desc":"TorrentMafya",
+        "url":"https://www.torrentmafya.net/?s={%query%}"
+    },
+    {
+        "name":"gthb",
+        "desc":"GitHub",
+        "url":"https://github.com/search?q={%query%}"
+    }
+];
+
+
+function save() {
+    browser.storage.local.set({"ssearch_sites":sites}, () => {
+        console.log("Saved!",sites);
+        sites = sites;
+    });
+}
+
+function load() {
+    browser.storage.local.get('ssearch_sites', (res) => {
+        console.log("Got!",res.ssearch_sites);
+        sites = res.ssearch_sites;
+    });
+}
+
+browser.runtime.onInstalled.addListener((details) => {
+    if (details.reason == "install") {
+        console.log("First install");
+        save();
+        load();
+    }
+});
+
+load();
+
+function createURL(site,query) {
+    for (var i = 0; i < sites.length; i++) {
+        if ( sites[i].name == site ) {
+            return `${sites[i].url.replace("{%query%}",query)}`
+        }
+    }
+}
+
+function dataChanged() {
+	save();
+	load();
+}
+
+browser.omnibox.onInputEntered.addListener((text, disposition) => {
+    disposition = "currentTab";
+    var arr, site, query;
+    arr = text.split(' ');
+    let operation = arr[0];
+    console.log(sites);
+    if (operation === "+" ) {
+        let url = arr[arr.length - 1];
+        arr.pop();
+        arr.shift();
+        let name = arr.join(' ');
+        sites.push(
+            {
+                "name":`${operation.substring(1)}`,
+                "desc":name,
+                "url":url
+            }
+        );
+				dataChanged();
+    }
+    else if (operation === "-") {
+        for (var i = 0; i < sites.length; i++) {
+            if ( sites[i].name == operation.substring(1) ) {
+                sites.splice(i,1);
+            }
+        }
+				dataChanged();
+    }
+    else if (operation === "<=" ) {
+        let temp = text.substring(text.indexOf("["));
+        let temp_sites = JSON.parse(temp);
+        sites = temp_sites;
+				dataChanged();
+    }
+    else if (operation === "+<=" ) {
+        let temp = text.substring(text.indexOf("["));
+        let temp_sites = JSON.parse(temp);
+        for (var i = 0; i < temp_sites.length; i++) {
+            sites.push(temp_sites[i]);
+        }
+				dataChanged();
+    }
+    else {
+        if ( arr.length > 1 ) {
+            site = arr[0].toLowerCase().trim();
+            arr.shift();
+            query = arr.join(" ").trim();
+            var createdurl = createURL(site,query);
+            browser.tabs.update({
+                url: createdurl
+            });
+        }
+    }
+});
+
+function rawSuggest(site, query) {
+    return [
+        { content: `${site}/${query}`, description: `Search on ${site} for ${query}` },
+        { content: `${site}.com/${query}`, description: `Search on ${site}.com/ for ${query}` },
+        { content: `${site}.net/${query}`, description: `Search on ${site}.net/ for ${query}` },
+        { content: `${site}.org/${query}`, description: `Search on ${site}.org/ for ${query}` },
+    ];
+}
+
+browser.omnibox.onInputChanged.addListener((text, suggest) => {
+    text = text.trim();
+    if ( text == "." ) {
+        let suggestions = [];
+        sites.forEach(e => {
+            let temp = {
+                content: `${e.url}`, description: `Search on ${e.name} (${e.desc})`
+            };
+            suggestions.push(temp);
+        });
+        suggest(suggestions);
+    }
+    else if ( text == "..." ) {
+        let content = "";
+        for (let i=0; i < sites.length; i++) {
+            if ( i == sites.length - 1) {
+                content += sites[i].name;
+            }
+            else content += `${sites[i].name}, `;
+        }
+        suggest(
+            [
+                {
+                    content: content, description: content
+                }
+            ]
+        );
+    }
+    else if ( text == "=>" ) {
+        load();
+        console.log(sites);
+        suggest(
+            [
+                {
+                    content: JSON.stringify(sites), description: "Export to JSON"
+                }
+            ]
+        );
+    }
+    else {
+        var arr, site, query;
+        arr = text.split(' ');
+        if ( arr.length > 1) {
+            site = arr[0].toLowerCase().trim();
+            arr.shift();
+            query = arr.join(" ").trim();
+
+            let suggestions = [];
+
+            sites.forEach(e => {
+                let temp = {
+                    content: "",
+                    description: ""
+                };
+                if (e.name === site || e.name.includes(site)) {
+                    temp.content = `${e.url}${query}`;
+                    temp.description = `Search for ${query} on ${e.desc}`;
+                    suggestions.push(temp);
+                }
+                else {
+                    rawSuggest(site,query).forEach(e=>suggestions.push(e));
+                }
+            });
+            suggest(suggestions);
+        }
+    }
+});
